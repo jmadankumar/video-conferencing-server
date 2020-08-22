2b1cd04821cc9197b7404b658442f2bd65c3da6a
Madan Kumar <kumarmadan.j@gmail.com>
2020-08-22 17:43:20 +0530

    build script for deployment

---
+++ scripts/build.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const archiver = require('archiver');
+const copy = require('./copy');
+
+const workingDir = process.cwd();
+const dirToZip = path.resolve(workingDir, 'build');
+
+async function zip() {
+    return new Promise((resolve, reject) => {
+        const output = fs.createWriteStream(path.join(workingDir, 'build.zip'));
+        const archive = archiver('zip', { zlib: { level: 9 } });
+        output.on('close', function () {
+            resolve();
+        });
+        archive.on('error', function (err) {
+            reject(err);
+        });
+        archive.directory(dirToZip, false).pipe(output);
+
+        archive.finalize();
+    });
+}
+
+async function build() {
+    await copy();
+    await zip();
+}
+
+build();
---
+++ scripts/copy.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const fs = require('fs');
+
+const destination = 'build';
+const filesToCopy = ['package.json', 'yarn.lock'];
+
+async function copy() {
+    const dir = process.cwd();
+    console.log();
+    filesToCopy.forEach((file) => {
+        fs.copyFileSync(path.resolve(dir, file), path.resolve(dir, destination, file));
+    });
+}
+module.exports = copy;
